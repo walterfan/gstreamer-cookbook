884866366b065404bb44d5474327686e2eca3aea
Walter Fan <walter.fan@gmail.com>
2023-10-19 23:28:54 +0800

    update example

--- example/hls-exam.cpp
+++ example/hls-exam.cpp
@@ -1,20 +1,7 @@
-#include <gst/gst.h>
-#include <glib.h>
 #include <chrono>
 #include <thread>
-#include <iostream>
-#include <chrono>  // chrono::system_clock
-#include <ctime>   // localtime
-#include <sstream> // stringstream
-#include <iomanip> // put_time
-#include <string>  // string
-#include <fmt/core.h>
 #include "pipeline_controller.h"
 
-/*
-gst-launch-1.0 avfvideosrc device-index=1 ! \
-video/x-raw,width=1920,height=1080,format=UYVY,framerate=30/1 ! autovideosink
-*/
 int main(int argc, char *argv[]) {
     auto controller = std::make_unique<PipelineController>();
     controller->init(argc, argv);
